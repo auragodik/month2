e63dd51ba950c71c2c1c8330a8943e72ea7302e6
auragodik <nuradilkarybaev6@gmail.com>
2025-09-05 16:34:40 +0600

    ДЛя показов примера

---
+++ lesson_1.py
@@ -0,0 +1,20 @@
+# Основы ООП, Создание первых классов, Атрибуты и Методы классов, Принцип ООП - Наследование.
+# class Car:
+#     #инициализатор
+#     def __init__(self, color, model):
+#         self.color = color
+#         self.model = model
+#
+#     def drive_to_location(self, location):
+#         print(f'Car {self.model} is driving to {location}')
+# car_honda = Car("silver", 'honda')
+# print(car_honda)
+# car_subaru = Car('black', 'subaru')
+# print(car_subaru)
+# car_subaru.drive_to_location("Bishkek")
+# print(f'Car color {car_subaru.color}, model {car_subaru.model}')
+# car_honda.color = "RED"
+# print(type(car_honda))
+# print(type(123123))
+# print(type('spisladko'))
+# print(car_honda.color)
