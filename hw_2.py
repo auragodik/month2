837f238d6496a42bf531d4a3b6ddfab140588d03
auragodik <nuradilkarybaev6@gmail.com>
2025-09-07 15:19:40 +0600

    основная задача, все по условиям, код корректный

--- hw_2.py
+++ hw_2.py
@@ -6,25 +6,26 @@ class Person:
     def introduce(self):
             print(f"Меня зовут {self.name}, мне {self.birth_date} лет, я занимаюсь {self.occupation}.")
 class Classmate(Person):
-    def __init__(self, name, birth_date, occupation, group_name):
+    def __init__(self, name, birth_date, occupation):
         self.name = name
         self.birth_date = birth_date
         self.occupation = occupation
-        self.group_name = group_name
     def introduce(self):
-        print(f"Меня зовут {self.name}, я одноклассник Нурадила, мне {self.birth_date} лет, я занимаюсь {self.occupation}, учились в {self.group_name}.")
+        print(f"Меня зовут {self.name}, я одноклассник Нурадила, мне {self.birth_date} лет, я занимаюсь {self.occupation}.")
 class Friend(Person):
-    def __init__(self, name, birth_date, occupation, hobby):
+    def __init__(self, name, birth_date, occupation):
         self.name = name
         self.birth_date = birth_date
         self.occupation = occupation
-        self.hobby = hobby
     def introduce(self):
-        print(f"Меня зовут {self.name}, я друг Нурадила, мне {self.birth_date} лет, я занимаюсь {self.occupation}, хобби и есть {self.hobby}.")
-cl1 = Classmate('Чынгыз', '17', 'луксмаксингом', '11Ж')
-cl2 = Classmate('Барсбек', '17', 'учебой', '11Ж')
-fr1 = Friend('Арсен', '16', 'чтением книг', "чтение книг")
-fr2 = Friend("Эрасыл", "17", 'моделингом', "моделинг")
-fr3 = Friend('Азирет', "17", 'боксом', 'бокс')
-for prsn in [cl1, cl2, fr1, fr2, fr3]:
-    prsn.introduce()
+        print(f"Меня зовут {self.name}, я друг Нурадила, мне {self.birth_date} лет, я занимаюсь {self.occupation}.")
+cl1 = Classmate('Чынгыз', '17', 'луксмаксингом')
+cl2 = Classmate('Барсбек', '17', 'учебой')
+fr1 = Friend('Арсен', '16', 'чтением книг')
+fr2 = Friend("Эрасыл", "17", 'моделингом')
+fr3 = Friend('Азирет', "17", 'боксом')
+cl1.introduce()
+cl2.introduce()
+fr1.introduce()
+fr2.introduce()
+fr3.introduce()
