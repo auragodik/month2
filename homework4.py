fe93af112d265862560dde053b8b1074d9da1969
auragodik <nuradilkarybaev6@gmail.com>
2025-09-15 01:13:40 +0600

    hw4

---
+++ homework4.py
@@ -0,0 +1,21 @@
+
+class Vehicle:
+    def start(self):
+        print("Vehicle starting")
+
+class Car(Vehicle):
+    def start(self):
+        super().start()
+        print("Car starting")
+
+class ElectricCar(Vehicle):
+    def start(self):
+        super().start()
+        pass
+
+class Tesla(ElectricCar, Car):
+    def start(self):
+        super().start()
+        print("Tesla ready")
+# print(Tesla.__mro__)
+Tesla().start()
