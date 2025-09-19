88bffeac9988a496c90d7caba1f3bec755a4ab5e
auragodik <nuradilkarybaev6@gmail.com>
2025-09-19 16:52:36 +0600

    исправил

--- hws/distance.py
+++ hws/distance.py
@@ -1,27 +1,28 @@
-class Distance():
-    def __init__(self, value, unit = 'm'):
-        units = {
-            "mm": 0.001,
-            "cm": 0.01,
-            "m": 1,
-            "km": 1000
-        }
+class Distance:
+    units = {
+        "mm": 0.001,
+        "cm": 0.01,
+        "m": 1,
+        "km": 1000
+    }
+
+    def __init__(self, value, unit="m"):
         self.value = value
         self.unit = unit
+
     def to_meters(self):
-        return self.value * self.units[self.unit]
+        return Distance(self.value * self.units[self.unit], "m")
+
     def __str__(self):
-        return f'{self.value}, {self.unit}'
+        return f"{self.to_meters().value} m"
+
     def __add__(self, other):
-        total_m = self.to_meters() + other.to_meters
-        return total_m(self.value + other.value)
+        total_m = self.to_meters().value + other.to_meters().value
+        return Distance(total_m / Distance.units[self.unit], self.unit)
+
     def __sub__(self, other):
-        diff_m = self.to_meters() - other.to_meters()
+        diff_m = self.to_meters().value - other.to_meters().value
         if diff_m < 0:
-            print("Результат не может быть отрицательным")
-        return Distance(self.value - other.value)
-    
-
-
-
+            raise ValueError("Результат не может быть отрицательным")
+        return Distance(diff_m / Distance.units[self.unit], self.unit)
 
--- hws/homework_5.py
+++ hws/homework_5.py
@@ -1,12 +1,12 @@
-from homeworks.distance import Distance
+from hws.distance import Distance
 
 d1 = Distance(100, "m")
 d2 = Distance(2, "km")
 d3 = Distance(50, "cm")
 
-print(d1)       # 100 m
-print(d2)       # 2 km
-print(d3)       # 50 cm
+print(d1)
+print(d2)
+print(d3)
 
-print(d1 + d2)  # 2100 m
-print(d2 - d1)  # 1900 km
+print(d1 + d2)
+print(d2 - d1)
